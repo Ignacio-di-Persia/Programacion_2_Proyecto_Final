946fdf544121ce68ca4a80a4a3d7cfbee35737d8
Catalina Galgani <catalinagalgani@alu.inspt.utn.edu.ar>
2025-11-18 16:11:34 -0300

    Modificacion de creacion de BBDD

--- Creacion_bbdd.sql
+++ Creacion_bbdd.sql
@@ -1,17 +1,3 @@
-CREATE DATABASE IF NOT EXISTS guarderia_central;
+DROP DATABASE guarderia_central;
+CREATE DATABASE guarderia_central;
 USE guarderia_central;
-
--- INSERT de datos para pruebas 
-INSERT INTO zonas (codigo, ancho, cantidad_vehiculos, profundidad, tipo_vehiculos)
-VALUES
-('A', 10, 5, 20, 'Autos'),
-('B', 8, 3, 15, 'Motos'),
-('C', 12, 7, 25, 'Camionetas');
-
-INSERT INTO garage (codigo, ocupado, zona_codigo)
-VALUES
-('G1', FALSE, 'A'),
-('G2', FALSE, 'A'),
-('G3', FALSE, 'B'),
-('G4', FALSE, 'C'),
-('G5', FALSE, 'C');
